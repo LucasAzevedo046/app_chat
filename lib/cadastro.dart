3bba96f0bed582c26e5e5681a313962c802af111
Juan D Lucas <juan.rodajau@gmail.com>
2026-05-20 19:23:18 -0300

    cadastro

--- lib/cadastro.dart
+++ lib/cadastro.dart
@@ -3,6 +3,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart' as path;
 
+
+
 class Cadastro extends StatefulWidget {
   const Cadastro({super.key});
 
